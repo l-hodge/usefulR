889122f9e7c27d8d168dbb5978f0e00ff49f38b9
Louis Hodge <Louis.HODGE@education.gov.uk>
2021-06-11 13:21:23 +0100

    Tweaks to .tex template and table style

--- R/dfe_table.R
+++ R/dfe_table.R
@@ -1,24 +1,33 @@
 #' DfE table styling
 #'
-#' @param df data.frame object to make into a LaTeX table
+#' Wrapper for knitr::kable to produce tables in the DfE style.
+#'
+#' @param df data.frame object to make into a DfE style table
+#' @param caption caption for the table
+#' @param label The table reference label. By default obtained from knitr chunk
+#' @param row.names include row names in table
+#' @param ... Other arguments passed to knitr::kable
 #'
 #' @importFrom dplyr "%>%"
 #' @importFrom knitr kable
-#' @importFrom kableExtra row_spec column_spec kable_styling
+#' @importFrom kableExtra row_spec column_spec
 #'
-#' @return A LaTeX table object.
+#' @return A LaTeX table object in the DfE style. Can also create DfE style-like HTML tables, although support is limited.
 #'
 #' @examples
 #' dfe_tbl(head(mtcars)[1:4])
 #'
 #' @export
 
-#\usepackage{colortbl}
-#\usepackage{xcolor}
+dfe_tbl <- function(df, label = knitr::opts_current$get('label'), caption = " ", row.names = TRUE, ...){
 
-dfe_tbl <- function(df){
-  knitr::kable(df, "latex", align =c("l")) %>%
-    kableExtra::row_spec(0, background = "#C5D4DE", bold=TRUE) %>%
-    kableExtra::column_spec(c(1,ncol(df)+1), latex_column_spec = c("|l", "l|")) %>%
-    kableExtra::kable_styling(latex_options="scale_down")
+  if(row.names == TRUE){
+    n = ncol(df)+1
+  } else {
+    n = ncol(df)
+  }
+
+  knitr::kable(df, align = c("l"), label = label, caption = caption, row.names = row.names, ...) %>%
+    kableExtra::row_spec(0, background = "#C5D4DE", bold = TRUE) %>%
+    kableExtra::column_spec(c(1, n), latex_column_spec = c("|l","l|"))
 }
